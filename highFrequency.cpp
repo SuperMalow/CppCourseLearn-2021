a69a309fc0f6b4368d10a9be691b9d059898ce52
Supermalow <229538252@qq.com>
2021-04-17 15:45:07 +0800

    second "增加了空载Q 负载Q 等效电阻 带宽 谐振电阻的重载"

--- highFrequency.cpp
+++ highFrequency.cpp
@@ -31,28 +31,54 @@ public:
         */
     }
     double calculateResonFrequency(double L, double C);
-    int calculateResonResistor(double L, double C, double r);
-    int calculateCap(double L, double resonFrequency);
-
+    double calculateResonResistor(double L, double C, double r);
+    double calculateResonResistor(double L,double C, double Q);
+    double calculateQ(double L, double C, double r);
+    double calculateCap(double L, double resonFrequency);
+    double calculateBandWidth(double resonFrequency);
+    double calculateEqualityResistor(double Rs, double Rp, double Rl);
+    double calculateLoadQ(double Re, double L, double C);
 };
 double HighFrequency::calculateResonFrequency(double L, double C)
 {
-    //计算谐振频率
+    //已知电感、电容 计算谐振频率
     return (1.0 / (2.0 * PI * (sqrt(L * C))));
 }
-int HighFrequency::calculateResonResistor(double L, double C, double r)
+double HighFrequency::calculateResonResistor(double L, double C, double r)
 {
-    //已知电感、电容、电感 计算内阻计算谐振电阻 L / r * C
+    //已知电感、电容、电感 计算谐振电阻 L / r * C
     return (L / r * C);
-
 }
-int HighFrequency::calculateCap(double L, double resonFrequency)
+double HighFrequency::calculateResonResistor(double L, double C, double Q)
+{
+    //已知电容、电感、Q值 计算谐振电阻 Rp = Q * sqrt(L/C)
+    return (Q * sqrt(L / C));
+}
+double HighFrequency::calculateQ(double L, double C, double r)
+{
+    //已知电感、电容、内阻  计算Q值
+    return ((1 / r) * sqrt(L / C));
+}
+double HighFrequency::calculateCap(double L, double resonFrequency)
 {
     //已知电感、谐振频率 计算电容 C = 1 / pow((2 * PI * f0),2) * L
     return (1 / pow((2 * PI * resonFrequency),2) * L);
 }
-
-
+double HighFrequency::calculateBandWidth(double resonFrequency)
+{
+    //已知谐振频率 计算带宽 BW = 2 * f0
+    return (2 * resonFrequency);
+}
+double HighFrequency::calculateEqualityResistor(double Rs, double Rp, double Rl)
+{
+    //已知信号源内阻、负载电阻、谐振电阻 计算电路等效电阻
+    return ((Rp * Rl + Rs * Rl + Rs * Rp) / (Rs * Rl * Rp));
+}
+double HighFrequency::calculateLoadQ(double Re, double L, double C)
+{
+    //已知等效电阻、电感、电容 计算有载Q值
+    return (Re * sqrt(C / L));
+}
 
 void test()
 {
