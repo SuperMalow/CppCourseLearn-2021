a23c997fbf69448317083ee908ed17c5a4bffbc6
Supermalow <229538252@qq.com>
2021-04-22 15:21:24 +0800

    master

---
+++ lab6_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//使用动态内存分配生成动态数组来完成lab6_1 并且使用指针实现函数功能。
+//使用new在堆区创建内存
+void mkdirarray()
+{
+
+
+}
+
+
+
+int main()
+{
+
+
+    system("pause");
+    return 0;
+}
