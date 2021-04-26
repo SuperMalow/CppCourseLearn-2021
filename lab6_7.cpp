ef8a8d200d3398fb40ca4ae0ad458444e1729d9d
Supermalow <229538252@qq.com>
2021-04-26 20:10:51 +0800

    master

--- lab6_7.cpp
+++ lab6_7.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 using namespace std;
+#include <ctime>
 
 //修改实验4中的类
+class DayTime
+{
+private:
+    /* data */
+public:
+    DayTime(/* args */);
+    ~DayTime();
+};
+
+DayTime::DayTime(/* args */)
+{
+}
+
+DayTime::~DayTime()
+{
+}
+
 
 
 
 int main()
 {
-
-
 
     system("pause");
     return 0;
